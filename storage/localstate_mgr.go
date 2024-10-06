421a23e3fd5dfeb3f6b61d62f641da191058445a
ubuntu <ubuntu@localhost.localdomain>
2024-10-06 10:15:27 +0800

    add tss files

---
+++ storage/localstate_mgr.go
@@ -0,0 +1,262 @@
+package storage
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/binance-chain/tss-lib/ecdsa/keygen"
+	"github.com/libp2p/go-libp2p/core/peer"
+	maddr "github.com/multiformats/go-multiaddr"
+
+	"github.com/0xPellNetwork/go-tss/conversion"
+	"github.com/0xPellNetwork/go-tss/p2p"
+)
+
+const keyFragmentSeed = "TSS_FRAGMENT_SEED"
+
+// KeygenLocalState is a structure used to represent the data we saved locally for different keygen
+type KeygenLocalState struct {
+	PubKey          string                    `json:"pub_key"`
+	LocalData       keygen.LocalPartySaveData `json:"local_data"`
+	ParticipantKeys []string                  `json:"participant_keys"` // the paticipant of last key gen
+	LocalPartyKey   string                    `json:"local_party_key"`
+}
+
+// LocalStateManager provide necessary methods to manage the local state, save it , and read it back
+// LocalStateManager doesn't have any opinion in regards to where it should be persistent to
+type LocalStateManager interface {
+	SaveLocalState(state KeygenLocalState) error
+	GetLocalState(pubKey string) (KeygenLocalState, error)
+	SaveAddressBook(addressBook map[peer.ID]p2p.AddrList) error
+	RetrieveP2PAddresses() (p2p.AddrList, error)
+}
+
+// FileStateMgr save the local state to file
+type FileStateMgr struct {
+	folder      string
+	writeLock   *sync.RWMutex
+	encryptMode bool
+	passkey     []byte
+	keyGen      *KeygenLocalState
+}
+
+// NewFileStateMgr create a new instance of the FileStateMgr which implements LocalStateManager
+func NewFileStateMgr(folder string, password string) (*FileStateMgr, error) {
+	if len(folder) > 0 {
+		_, err := os.Stat(folder)
+		if err != nil && os.IsNotExist(err) {
+			if err := os.MkdirAll(folder, os.ModePerm); err != nil {
+				return nil, err
+			}
+		}
+	}
+	encryptMode := true
+	key, err := getFragmentSeed(password)
+	if err != nil {
+		encryptMode = false
+	}
+	return &FileStateMgr{
+		folder:      folder,
+		writeLock:   &sync.RWMutex{},
+		encryptMode: encryptMode,
+		passkey:     key,
+		keyGen:      nil,
+	}, nil
+}
+
+func (fsm *FileStateMgr) getFilePathName(pubKey string) (string, error) {
+	ret, err := conversion.CheckKeyOnCurve(pubKey)
+	if err != nil {
+		return "", err
+	}
+	if !ret {
+		return "", errors.New("invalid pubkey for file name")
+	}
+
+	localFileName := fmt.Sprintf("localstate-%s.json", pubKey)
+	if len(fsm.folder) > 0 {
+		return filepath.Join(fsm.folder, localFileName), nil
+	}
+	return localFileName, nil
+}
+
+// SaveLocalState save the local state to file
+func (fsm *FileStateMgr) SaveLocalState(state KeygenLocalState) error {
+	buf, err := json.Marshal(state)
+	if err != nil {
+		return fmt.Errorf("fail to marshal KeygenLocalState to json: %w", err)
+	}
+	filePathName, err := fsm.getFilePathName(state.PubKey)
+	if err != nil {
+		return err
+	}
+	data, err := fsm.encryptFragment(buf)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filePathName, data, 0o600)
+}
+
+// GetLocalState read the local state from file system
+func (fsm *FileStateMgr) GetLocalState(pubKey string) (KeygenLocalState, error) {
+	if fsm.keyGen != nil {
+		return *fsm.keyGen, nil
+	}
+	if len(pubKey) == 0 {
+		return KeygenLocalState{}, errors.New("pub key is empty")
+	}
+	filePathName, err := fsm.getFilePathName(pubKey)
+	if err != nil {
+		return KeygenLocalState{}, err
+	}
+	if _, err := os.Stat(filePathName); os.IsNotExist(err) {
+		return KeygenLocalState{}, err
+	}
+	filePathName = filepath.Clean(filePathName)
+
+	buf, err := ioutil.ReadFile(filePathName)
+	if err != nil {
+		return KeygenLocalState{}, fmt.Errorf("fail to read from file(%s): %w", filePathName, err)
+	}
+	pt, err := fsm.decryptFragment(buf)
+	if err != nil {
+		return KeygenLocalState{}, fmt.Errorf("fail to decrypt data: %w", err)
+	}
+	var localState KeygenLocalState
+	if err := json.Unmarshal(pt, &localState); nil != err {
+		return KeygenLocalState{}, fmt.Errorf("fail to unmarshal KeygenLocalState:%x %w", pt, err)
+	}
+	fsm.keyGen = &localState
+	return localState, nil
+}
+
+func (fsm *FileStateMgr) SaveAddressBook(address map[peer.ID]p2p.AddrList) error {
+	if len(fsm.folder) < 1 {
+		return errors.New("base file path is invalid")
+	}
+	filePathName := filepath.Join(fsm.folder, "address_book.seed")
+	var buf bytes.Buffer
+
+	for peer, addrs := range address {
+		for _, addr := range addrs {
+			// we do not save the loopback addr
+			if strings.Contains(addr.String(), "127.0.0.1") {
+				continue
+			}
+			record := addr.String() + "/p2p/" + peer.String() + "\n"
+			_, err := buf.WriteString(record)
+			if err != nil {
+				return errors.New("fail to write the record to buffer")
+			}
+		}
+	}
+	fsm.writeLock.Lock()
+	defer fsm.writeLock.Unlock()
+	return ioutil.WriteFile(filePathName, buf.Bytes(), 0o600)
+}
+
+func (fsm *FileStateMgr) RetrieveP2PAddresses() (p2p.AddrList, error) {
+	if len(fsm.folder) < 1 {
+		return nil, errors.New("base file path is invalid")
+	}
+	filePathName := filepath.Join(fsm.folder, "address_book.seed")
+	filePathName = filepath.Clean(filePathName)
+
+	_, err := os.Stat(filePathName)
+	if err != nil {
+		return nil, err
+	}
+	fsm.writeLock.RLock()
+	input, err := ioutil.ReadFile(filePathName)
+	if err != nil {
+		fsm.writeLock.RUnlock()
+		return nil, err
+	}
+	fsm.writeLock.RUnlock()
+	data := strings.Split(string(input), "\n")
+	var peerAddresses []p2p.Multiaddr
+	for _, el := range data {
+		// we skip the empty entry
+		if len(el) == 0 {
+			continue
+		}
+		addr, err := maddr.NewMultiaddr(el)
+		if err != nil {
+			return nil, fmt.Errorf("invalid address in address book %w", err)
+		}
+		peerAddresses = append(peerAddresses, addr)
+	}
+	return peerAddresses, nil
+}
+
+func (fsm *FileStateMgr) encryptFragment(plainText []byte) ([]byte, error) {
+	if !fsm.encryptMode {
+		return plainText, nil
+	}
+	block, err := aes.NewCipher(fsm.passkey)
+	if err != nil {
+		return nil, err
+	}
+	// Creating GCM mode
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	// Generating random nonce
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+	cipherText := gcm.Seal(nonce, nonce, plainText, nil)
+	return cipherText, nil
+}
+
+func (fsm *FileStateMgr) decryptFragment(buf []byte) ([]byte, error) {
+	if !fsm.encryptMode {
+		return buf, nil
+	}
+	block, err := aes.NewCipher(fsm.passkey)
+	if err != nil {
+		return nil, err
+	}
+	// Creating GCM mode
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	// Detached nonce and decrypt
+	nonce := buf[:gcm.NonceSize()]
+	buf = buf[gcm.NonceSize():]
+	plainText, err := gcm.Open(nil, nonce, buf, nil)
+	if err != nil {
+		return nil, err
+	}
+	return plainText, nil
+}
+
+func getFragmentSeed(password string) ([]byte, error) {
+	seedStr := os.Getenv(keyFragmentSeed)
+	if seedStr == "" {
+		if password == "" {
+			return nil, errors.New("empty fragment seed, please check password: " + password)
+		}
+		seedStr = password
+	}
+
+	h := sha256.New()
+	h.Write([]byte(seedStr))
+	seed := h.Sum(nil)
+	return seed, nil
+}
---
+++ storage/localstate_mgr_test.go
@@ -0,0 +1,135 @@
+package storage
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"github.com/binance-chain/tss-lib/ecdsa/keygen"
+	tnet "github.com/libp2p/go-libp2p-testing/net"
+	"github.com/libp2p/go-libp2p/core/peer"
+	maddr "github.com/multiformats/go-multiaddr"
+	"github.com/0xPellNetwork/go-tss/conversion"
+	"github.com/0xPellNetwork/go-tss/p2p"
+	. "gopkg.in/check.v1"
+)
+
+type FileStateMgrTestSuite struct{}
+
+var _ = Suite(&FileStateMgrTestSuite{})
+
+func TestPackage(t *testing.T) { TestingT(t) }
+
+func (s *FileStateMgrTestSuite) SetUpTest(c *C) {
+	_ = os.Setenv(keyFragmentSeed, "vSeb9Pw5GEAcZHVX1AgxPPWeyPLWTfWkGWNXeDKFO2zHoqDsRnVVoLE4fEIcO14d")
+	conversion.SetupBech32Prefix()
+}
+
+func (s *FileStateMgrTestSuite) TestNewFileStateMgr(c *C) {
+	folder := os.TempDir()
+	f := filepath.Join(folder, "test", "test1", "test2")
+	defer func() {
+		err := os.RemoveAll(f)
+		c.Assert(err, IsNil)
+	}()
+	fsm, err := NewFileStateMgr(f, "password")
+	c.Assert(err, IsNil)
+	c.Assert(fsm, NotNil)
+	_, err = os.Stat(f)
+	c.Assert(err, IsNil)
+	fileName, err := fsm.getFilePathName("whatever")
+	c.Assert(err, NotNil)
+	fileName, err = fsm.getFilePathName("thorpub1addwnpepqf90u7n3nr2jwsw4t2gzhzqfdlply8dlzv3mdj4dr22uvhe04azq5gac3gq")
+	c.Assert(err, IsNil)
+	c.Assert(fileName, Equals, filepath.Join(f, "localstate-thorpub1addwnpepqf90u7n3nr2jwsw4t2gzhzqfdlply8dlzv3mdj4dr22uvhe04azq5gac3gq.json"))
+}
+
+func (s *FileStateMgrTestSuite) TestSaveLocalState(c *C) {
+	stateItem := KeygenLocalState{
+		PubKey:    "wasdfasdfasdfasdfasdfasdf",
+		LocalData: keygen.NewLocalPartySaveData(5),
+		ParticipantKeys: []string{
+			"A", "B", "C",
+		},
+		LocalPartyKey: "A",
+	}
+	folder := os.TempDir()
+	f := filepath.Join(folder, "test", "test1", "test2")
+	defer func() {
+		err := os.RemoveAll(f)
+		c.Assert(err, IsNil)
+	}()
+	fsm, err := NewFileStateMgr(f, "password")
+	c.Assert(err, IsNil)
+	c.Assert(fsm, NotNil)
+	c.Assert(fsm.SaveLocalState(stateItem), NotNil)
+	stateItem.PubKey = "thorpub1addwnpepqf90u7n3nr2jwsw4t2gzhzqfdlply8dlzv3mdj4dr22uvhe04azq5gac3gq"
+	c.Assert(fsm.SaveLocalState(stateItem), IsNil)
+	filePathName := filepath.Join(f, "localstate-"+stateItem.PubKey+".json")
+	_, err = os.Stat(filePathName)
+	c.Assert(err, IsNil)
+	item, err := fsm.GetLocalState(stateItem.PubKey)
+	c.Assert(err, IsNil)
+	c.Assert(reflect.DeepEqual(stateItem, item), Equals, true)
+}
+
+func (s *FileStateMgrTestSuite) TestSaveAddressBook(c *C) {
+	testAddresses := make(map[peer.ID]p2p.AddrList)
+	var t *testing.T
+	id1 := tnet.RandIdentityOrFatal(t)
+	id2 := tnet.RandIdentityOrFatal(t)
+	id3 := tnet.RandIdentityOrFatal(t)
+	mockAddr, err := maddr.NewMultiaddr("/ip4/192.168.3.5/tcp/6668")
+	c.Assert(err, IsNil)
+	peers := []peer.ID{id1.ID(), id2.ID(), id3.ID()}
+	for _, each := range peers {
+		testAddresses[each] = []p2p.Multiaddr{mockAddr}
+	}
+	folder := os.TempDir()
+	f := filepath.Join(folder, "test")
+	defer func() {
+		err := os.RemoveAll(f)
+		c.Assert(err, IsNil)
+	}()
+	fsm, err := NewFileStateMgr(f, "password")
+	c.Assert(err, IsNil)
+	c.Assert(fsm, NotNil)
+	c.Assert(fsm.SaveAddressBook(testAddresses), IsNil)
+	filePathName := filepath.Join(f, "address_book.seed")
+	_, err = os.Stat(filePathName)
+	c.Assert(err, IsNil)
+	item, err := fsm.RetrieveP2PAddresses()
+	c.Assert(err, IsNil)
+	c.Assert(item, HasLen, 3)
+}
+
+func (s *FileStateMgrTestSuite) TestEncryption(c *C) {
+	folder := os.TempDir()
+	f := filepath.Join(folder, "test", "test1", "test2")
+	defer func() {
+		err := os.RemoveAll(f)
+		c.Assert(err, IsNil)
+	}()
+	fsm, err := NewFileStateMgr(f, "password")
+	c.Assert(err, IsNil)
+	c.Assert(fsm, NotNil)
+
+	stateItem := KeygenLocalState{
+		PubKey:    "wasdfasdfasdfasdfasdfasdf",
+		LocalData: keygen.NewLocalPartySaveData(5),
+		ParticipantKeys: []string{
+			"A", "B", "C",
+		},
+		LocalPartyKey: "A",
+	}
+	buf, err := json.Marshal(stateItem)
+	c.Assert(buf, NotNil)
+
+	ct, err := fsm.encryptFragment(buf)
+	c.Assert(err, IsNil)
+	pt, err := fsm.decryptFragment(ct)
+	c.Assert(err, IsNil)
+	c.Assert(reflect.DeepEqual(buf, pt), Equals, true)
+}
